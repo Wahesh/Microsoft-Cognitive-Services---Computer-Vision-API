c42e57c0b90feeb0cbb72dd489ed4c38aef267cb
Mahesh Dahal <maheshdahal@live.com>
2017-01-25 17:15:58 +0545

    Update Form1.cs

--- WindowsFormsApplication3/Form1.cs
+++ WindowsFormsApplication3/Form1.cs
@@ -29,19 +29,20 @@ namespace WindowsFormsApplication3
         private async void test(string filename)
         {
            
-            var visionClient = new VisionServiceClient("b7428b0a2f14418697266156742ee67b");
+            var visionClient = new VisionServiceClient("<--Enter Code here from https://www.microsoft.com/cognitive-services/en-us/ -->");
             AnalysisResult analysisResult;var features = new VisualFeature[] { VisualFeature.Tags, VisualFeature.Description };
             var fs = new FileStream(@filename, FileMode.Open);
-            analysisResult = await visionClient.AnalyzeImageAsync(fs, features);
+            analysisResult = await visionClient.AnalyzeImageAsync(fs, features); //THis is the main api call
             fs.Dispose();
             pictureBox1.ImageLocation = filename;
-           string json = JsonConvert.SerializeObject(analysisResult);
-            textBox1.Text = json;
+           string json = JsonConvert.SerializeObject(analysisResult);//The API Returns data here
+            textBox1.Text = json; //Data is directly dumped into a textbox. You can do as your app requires.
             
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+        //THis is only for fileupload. 
             DialogResult result = openFileDialog1.ShowDialog(); // Show the dialog.
             if (result == DialogResult.OK) // Test result.
             {
